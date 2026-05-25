bbac60ae455257bb470c6db04d3025a9be79be0f
FleurVanDeVille <fleur.van.de.ville@student.ehb.be>
2026-05-26 00:56:17 +0200

    adding my start-screen and making sure that it's the first screen I will see when I open the app.

---
+++ saveBuddy/frontend/app/index.jsx
@@ -0,0 +1,59 @@
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { router } from "expo-router";
+
+export default function StartScreen() {
+	return (
+		<View style={styles.container}>
+			<View style={styles.logoContainer}>
+				<Image
+					source={require("../assets/images/logo-saveBuddy.png")}
+					style={styles.logo}
+					resizeMode="contain"
+				/>
+			</View>
+
+			<TouchableOpacity
+				style={styles.button}
+				onPress={() => router.push("/login")}
+			>
+				<Text style={styles.buttonText}>Start</Text>
+			</TouchableOpacity>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#12384C",
+		justifyContent: "space-between",
+		alignItems: "center",
+		paddingTop: 180,
+		paddingBottom: 34,
+		paddingHorizontal: 38,
+	},
+
+	logoContainer: {
+		alignItems: "center",
+	},
+
+	logo: {
+		width: 280,
+		height: 280,
+	},
+
+	button: {
+		width: "100%",
+		backgroundColor: "#F5F5F5",
+		paddingVertical: 15,
+		borderRadius: 12,
+		alignItems: "center",
+	},
+
+	buttonText: {
+		color: "#12384C",
+		fontSize: 18,
+		fontWeight: "700",
+	},
+});
