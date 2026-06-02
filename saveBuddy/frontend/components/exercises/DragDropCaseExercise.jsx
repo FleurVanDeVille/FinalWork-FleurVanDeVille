5866cb75f23fa3c48f77eee6a516196c3730ed1e
FleurVanDeVille <fleur.van.de.ville@student.ehb.be>
2026-06-02 15:38:34 +0200

    feat (WIP): add additional images and enhance layout for drag-and-drop exercise

--- saveBuddy/frontend/components/exercises/DragDropCaseExercise.jsx
+++ saveBuddy/frontend/components/exercises/DragDropCaseExercise.jsx
@@ -2,6 +2,14 @@ import { Image, StyleSheet, Text, View } from "react-native";
 
 const images = {
 	"koffer.png": require("../../assets/images/ehbo-koffer.png"),
+	"pijnstillers.png": require("../../assets/images/pijnstillers.png"),
+	"pincet.png": require("../../assets/images/pincet.png"),
+	"schaar.png": require("../../assets/images/schaar.png"),
+	"pleisters.png": require("../../assets/images/pleisters.png"),
+	"water.png": require("../../assets/images/water.png"),
+	"wondgaas.png": require("../../assets/images/wondgaas.png"),
+	"watten.png": require("../../assets/images/watten.png"),
+	"thermometer.png": require("../../assets/images/thermometer.png"),
 };
 
 export default function DragDropCaseExercise({ exercise }) {
@@ -10,12 +18,24 @@ export default function DragDropCaseExercise({ exercise }) {
 			<Text style={styles.question}>{exercise.question}</Text>
 
 			<View style={styles.caseContainer}>
-                <Text style={styles.caseText}>Sleep de juiste materialen in de koffer:</Text>
+				<Text style={styles.caseText}>
+					Sleep de juiste materialen in de koffer:
+				</Text>
 				<Image
 					source={images[exercise.image]}
 					style={styles.caseImage}
 					resizeMode="contain"
 				/>
+			</View>
+
+			<View style={styles.itemsContainer}>
+				{exercise.items.map((item) => (
+					<View key={item.id} style={styles.item}>
+						<Image source={images[item.image]} style={styles.itemImage} />
+
+						<Text style={styles.itemText}>{item.text}</Text>
+					</View>
+				))}
 			</View>
 		</View>
 	);
@@ -39,5 +59,31 @@ const styles = StyleSheet.create({
 	caseImage: {
 		width: 220,
 		height: 180,
+	},
+
+	itemsContainer: {
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "space-between",
+		marginTop: 30,
+	},
+
+	item: {
+		width: "23%",
+		alignItems: "center",
+		marginBottom: 20,
+	},
+
+	itemImage: {
+		width: 55,
+		height: 55,
+	},
+
+	itemText: {
+		marginTop: 4,
+		fontSize: 12,
+		fontWeight: "700",
+		color: "#12384C",
+		textAlign: "center",
 	},
 });
